e2ad3f4a3f87daf8fae2aa997dbc8968846ff225
russellelliott <77514172+russellelliott@users.noreply.github.com>
2023-02-17 18:51:16 -0800

    cleanup

--- app/cdk-drag-drop-connected-sorting-group-example.ts
+++ app/cdk-drag-drop-connected-sorting-group-example.ts
@@ -11,55 +11,6 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class CdkDragDropConnectedSortingGroupExample {
 
-  /*groups = [{
-    id: 1,
-    title: 'Group 1',
-    items: [{
-      name: 'Item 1 - Group 1'
-    },
-    {
-      name: 'Item 2 - Group 1'
-    },
-    {
-      name: 'Item 3 - Group 1'
-    },
-    {
-      name: 'Item 4 - Group 1'
-    }]
-  },
-  {
-    id: 2,
-    title: 'Group 2',
-    items: [{
-      name: 'Item 1 - Group 2'
-    },
-    {
-      name: 'Item 2 - Group 2'
-    },
-    {
-      name: 'Item 3 - Group 2'
-    },
-    {
-      name: 'Item 4 - Group 2'
-    }]
-  },
-  {
-    id: 3,
-    title: 'Group 3',
-    items: [{
-      name: 'Item 1 - Group 3'
-    },
-    {
-      name: 'Item 2 - Group 3'
-    },
-    {
-      name: 'Item 3 - Group 3'
-    },
-    {
-      name: 'Item 4 - Group 3'
-    }]
-  }];*/
-
   groups = [
     {
       id: 0,
